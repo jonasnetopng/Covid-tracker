8f18b7def15a23b3c3c8c39dfbcc844b0bad264e
Jonas <jonaseluna@gmail.com>
2022-01-28 14:59:42 -0300

    format numbers, and update to hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios"
 import "./style.css"
-export default class App extends React.Component{
-  state = {
-    confirmed: 0,
-    recovered: 0,
-    deaths: 0,
+export default function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    async function getData() {
+      const res = await Axios.get("https://covid19.mathdro.id/api");
+      setData({
+        confirmed: res.data.confirmed.value,
+        recovered: res.data.recovered.value,
+        deaths: res.data.deaths.value
+      })
+    }
+    getData()
+  }, [])
+  const formatNum = (x) => {
+    const num = x.toString()
+    return num.replace(/\B(?=(\d{3})+(?!\d))/g, ".")
   }
-
-  componentDidMount(){
-    this.getData();
-  }
-
-  async getData(){
-    const res = await Axios.get("https://covid19.mathdro.id/api");
-    this.setState({
-      confirmed: res.data.confirmed.value,
-      recovered: res.data.recovered.value,
-      deaths: res.data.deaths.value
-    })
-  }
-
-  //this.state.deaths
-  render(){
-    return(
-      <div>
-        <div className="header">
-          <h1>COVID-19</h1>
+  return (
+    <div>
+      <div className="header">
+        <h1>COVID-19</h1>
       </div>
-    <div className="center">
-    <div className="info-container">
-                <h3>Confirmados</h3>
-                <h2>{this.state.confirmed}</h2>
-              </div>
-              <div className="info-container">
-                <h3>Recuperados</h3>
-                <h2>{this.state.recovered}</h2>          </div>
-              <div className="info-container">
-                <h3>Mortos</h3>
-                <h2>{this.state.deaths}</h2>          </div>
-          </div>
+      <div className="center">
+        <div className="info-container">
+          <h3>Confirmados</h3>
+          <h2>{formatNum(data.confirmed)}</h2>
+        </div>
+        <div className="info-container">
+          <h3>Mortos</h3>
+          <h2>{formatNum(data.deaths)}</h2>
+        </div>
+      </div>
     </div>
-    )
-  }
+  )
 }
